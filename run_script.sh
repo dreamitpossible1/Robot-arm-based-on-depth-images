1febf6e8cc782fa8eb097ede5f037f29af18b41e
dreamitpossible1 <68002596+dreamitpossible1@users.noreply.github.com>
2025-06-18 15:15:14 +0800

    Create run_script.sh

---
+++ run_script.sh
@@ -0,0 +1,10 @@
+export XDG_RUNTIME_DIR=/dev/socket/weston && export WAYLAND_DISPLAY=wayland-1
+
+
+
+python3 /usr/bin/gst-ai-object-detection.py \
+  -f 2 \
+  -ml yolov8 \
+  -m /opt/yamnet.tflite \
+  -l /opt/yolov8.labels \
+  -k "YOLOv8,q-offsets=<-107.0, -128.0, 0.0>,q-scales=<3.093529462814331, 0.00390625, 1.0>"
