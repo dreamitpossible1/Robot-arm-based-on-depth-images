3bf1cde5e31c3ba130eaf22a9c86bfc88e42579f
dreamitpossible1 <68002596+dreamitpossible1@users.noreply.github.com>
2025-07-02 10:15:36 +0800

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,4 +1,6 @@
 export XDG_RUNTIME_DIR=/dev/socket/weston && export WAYLAND_DISPLAY=wayland-1
 
 
- gst-ai-monodepth --config-file=/opt/config_monodepth.json
+
+python get_dep.py -f 2 -ml midas-v2 --model /opt/midas_quantized.tflite --labels /opt/monodepth.labels --constants "Midas,q-offsets=<0.0>,q-scales=<6.846843242645264>;"
+
